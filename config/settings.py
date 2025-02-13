0c1c779a507c25c6cb99857477994c9066cf6198
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2025-02-14 00:41:59 +0300

    Debug check false

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
