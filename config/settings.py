c4445d369976b66eec403c35fb923ee264db652f
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2025-02-14 00:47:51 +0300

    Debug check false

--- config/settings.py
+++ config/settings.py
@@ -30,13 +30,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
     'bakery',
     'cart',
     'orders',
